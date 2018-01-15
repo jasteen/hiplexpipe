dbc07e013695c9ba0d75cf0465fec89a29636362
Jason Steen <jason.steen@monash.edu>
2018-01-16 10:23:11 +1100

    typos

--- src/pipeline.py
+++ src/pipeline.py
@@ -135,7 +135,7 @@ def make_pipeline(state):
         task_func=stages.generate_stats,
         name='generate_stats',
         regex(r'(.+).(.+).txt'),
-        inputs=['\1.bedtools_hist_all.txt', '\1.mapped_to_genome.txt', '\1.mapped_to_target.txt', '\1.total_raw_reads.txt'], 
+        input=['\1.bedtools_hist_all.txt', '\1.mapped_to_genome.txt', '\1.mapped_to_target.txt', '\1.total_raw_reads.txt'], 
         extras=['\1'],
         output='all_sample.summary.txt')
 
