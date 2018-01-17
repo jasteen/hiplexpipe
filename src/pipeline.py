676b1b576e0f20a1b8c66fe81bfaa60874c60a7a
Jason Steen <jason.steen@monash.edu>
2018-01-17 12:01:26 +1100

    trying merge and tidying up

--- src/pipeline.py
+++ src/pipeline.py
@@ -122,7 +122,7 @@ def make_pipeline(state):
         filter=suffix('.clipped.bam'),
         output='.total_raw_reads.txt')
 
-    pipeline.merge(
+    pipeline.transform(
         task_func=stages.generate_stats,
         name='generate_stats',
         input=output_from(['coverage_bed', 'genome_reads', 'target_reads', 'total_reads']), 
